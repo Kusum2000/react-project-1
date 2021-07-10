b5c2128b0a5356cc27db5e52db8bad72c5c58593
Kusum2000 <kusumrch@gmail.com>
2021-07-10 17:23:12 +0530

    contact me page

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,7 @@ function Contact(){
     return(
         <div className="contact-div">
             <h2>Contact Me</h2>
-            <p>Lets get in touch via...</p>
+            <p>Get in touch via...</p>
             <p><b><small>WhatsApp, Instagram, LinkedIn </small></b>:)</p>
         </div>
     )
